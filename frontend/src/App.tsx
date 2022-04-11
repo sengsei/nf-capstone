713ab05aa71f18223abfad3ac39320b2b21dd596
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-04-11 12:05:13 +0200

    routing fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,12 @@
 import Header from "./components/Header";
-import {Outlet, useNavigate} from "react-router-dom";
-import {useEffect} from "react";
-
-
+import {Outlet} from "react-router-dom";
 
 function App() {
 
-    const navigate = useNavigate()
-
-    const routeToPath = (where: string) => {
-        navigate("/" + where)
-    }
-
-    useEffect(() => {
-        routeToPath("home")
-    }, [])
-
     return (
         <div>
-
-                <Header/>
-                <Outlet/>
-
+            <Header/>
+            <Outlet/>
         </div>
     );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense }from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -17,23 +17,23 @@ ReactDOM.render(
     <React.StrictMode>
         <Suspense fallback={"Loading..."}>
             <BrowserRouter>
-            <Routes>
-                <Route path={'/'} element={<App/>}>
-                <Route path={'/home'} element={<LandingPage/>}/>
-                <Route path={'/categories'} element={<CategoryList/>}/>
-                <Route path={'/questions/:categoryName'} element={<QuestionListTrueFalse/>}/>
-                <Route path={'/editormenue'} element={<EditorMenue/>}/>
-                <Route path={'/truefalseedit'} element={<TrueFalseEditor/>}/>
-                <Route path={'/editorcategories'} element={<EditorCategories/>}/>
-                <Route path={'/questions/trivia-tf'} element={<TriviaTrueFalse/>}/>
-                <Route path={'/login'} element={<Login/>}/>
-                <Route path={'/register'} element={<Register/>}/>
-                </Route>
-            </Routes>
+                <Routes>
+                    <Route path={'/'} element={<App/>}>
+                        <Route path={'/home'} element={<LandingPage/>}/>
+                        <Route path={'/categories'} element={<CategoryList/>}/>
+                        <Route path={'/questions/:categoryName'} element={<QuestionListTrueFalse/>}/>
+                        <Route path={'/editormenue'} element={<EditorMenue/>}/>
+                        <Route path={'/truefalseedit'} element={<TrueFalseEditor/>}/>
+                        <Route path={'/editorcategories'} element={<EditorCategories/>}/>
+                        <Route path={'/questions/trivia-tf'} element={<TriviaTrueFalse/>}/>
+                        <Route path={'/login'} element={<Login/>}/>
+                        <Route path={'/register'} element={<Register/>}/>
+                    </Route>
+                </Routes>
             </BrowserRouter>
         </Suspense>
     </React.StrictMode>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
