52f43deba64150dc6b48f93008f0b8b4c309cb1a
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-04-12 09:40:00 +0200

    logout better

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     }
 
     const logout = () => {
-        localStorage.setItem("token", "")
+        localStorage.removeItem("token")
         navigate("/login")
     }
 
