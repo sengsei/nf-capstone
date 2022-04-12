31addd9148f8a83f20592442f7f6ef24ca8251be
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-04-12 22:04:02 +0200

    first css steps

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -14,13 +14,13 @@ export default function Header() {
     }
 
     return(
-        <div>
+        <div className={"flex space-x-4 bg-lime-500"}>
             <div>TRUTHY </div>
-            <button onClick={() => routeToPath('login')}>Login</button>
-            <button onClick={() => logout()}>Logout</button>
-            <button onClick={() => routeToPath('home')}>Home</button>
-            <button>Konto</button>
-            <button onClick={() => routeToPath('editormenue')}>Editor</button>
+            <div><button className={"font-bold text-amber-400"} onClick={() => routeToPath('login')}>Login</button></div>
+            <div><button className={"font-bold text-amber-400"} onClick={() => logout()}>Logout</button></div>
+            <div><button className={"font-bold text-amber-400"} onClick={() => routeToPath('home')}>Home</button></div>
+            <div><button className={"font-bold text-amber-400"}>Konto</button></div>
+            <div><button className={"font-bold text-amber-400"} onClick={() => routeToPath('editormenue')}>Editor</button></div>
         </div>
     )
 }
--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -13,13 +13,17 @@ export default function LandingPage() {
 
     return (
         <div>
-            <div>
+            <div className={"columns-2 bg-lime-700 font-sans text-amber-400"}>
                 TRUTHY ist ein kostenloses Webangebot für Studierende, die ihr Programmierwissen abfragen und erweitern möchten.
                 Mit true/false Fragen wird geprüft was Du schon kannst und was nochmal gelernt werden sollte.
                 Mit dem TRUTHY Editor kannst Du deine individuellen Fragen hinzufügen.
+                <img alt={"Schüler mit Buch"} width={"200"} height={"200"} src={"https://images.pexels.com/photos/2781195/pexels-photo-2781195.jpeg?cs=srgb&dl=pexels-kaushal-moradiya-2781195.jpg&fm=jpg"}/>
             </div>
+            <div className={"columns-2"}>
                 <button onClick={() => routeToPath("categories")}>Start</button>
                 <button onClick={() => routeToPath("register")}>Registrieren</button>
+            </div>
+
         </div>
     )
 }
